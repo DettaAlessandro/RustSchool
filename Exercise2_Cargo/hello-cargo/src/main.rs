52ca9f5443fe58f523e867f023fb6acec0c64291
AlessandroDetta3BHIF <alessandro.detta04@gmail.com>
2024-08-21 13:09:31 +0200

    Initial Commit with 2 exercises

---
+++ Exercise2_Cargo/hello-cargo/src/main.rs
@@ -0,0 +1,9 @@
+//to create a new rust project with cargo use carge new "project"
+//to build a project use cargo build
+//to run it use cargo run 
+//cargo checks only checks if it compiles but does not create the executable, it is much
+//faster
+//use cargo build --release if you are 100% ready to release the program will have a faster run time
+fn main() { 
+    println!("Hello, world!");
+}
