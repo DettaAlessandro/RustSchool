c86bce72e876d41aea81b9bae86bb2042f592941
AlessandroDetta3BHIF <alessandro.detta04@gmail.com>
2024-08-21 13:19:09 +0200

    Corrected Names

---
+++ exercise3-guessing-game/src/main.rs
@@ -0,0 +1,11 @@
+use std::io; // import io library
+
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new() // mut is for the mutability of a variable
+    io::stding()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {}", guess)
+}
