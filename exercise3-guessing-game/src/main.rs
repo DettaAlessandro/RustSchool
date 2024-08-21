772c71337cdfebd654d36a936ebd2efe2b6eaf76
AlessandroDetta3BHIF <alessandro.detta04@gmail.com>
2024-08-21 19:06:51 +0200

    More exercises"

--- exercise3-guessing-game/src/main.rs
+++ exercise3-guessing-game/src/main.rs
@@ -3,8 +3,8 @@ use std::io; // import io library
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
-    let mut guess = String::new() // mut is for the mutability of a variable
-    io::stding()
+    let mut guess = String::new();// mut is for the mutability of a variable
+    io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
     println!("You guessed: {}", guess)
